66c73e8d9be1db2f802021b937ffecb54d559b9e
s30291-pj <s30291@pjwstk.edu.pl>
2025-01-09 01:23:15 +0100

    further implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,13 +52,25 @@ class Contact {
     }
 }
 
+class Message {
+    sender;
+    content;
+    timestamp;
+
+    constructor(sender, content, timestamp) {
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+}
 
 class App {
-    accountContainer = new AccountContainer();
-    contactsContainer = new ContactsContainer;
+    accountContainer = new AccountContainer(this);
+    contactsContainer = new ContactsContainer(this);
+    chatContainer = new ChatContainer(this);
 
     credentials;
-    
+
     async init(credentials) {
         if(credentials instanceof Credentials) {
             this.credentials = credentials;
@@ -72,15 +84,18 @@ class App {
             this.contactsContainer.initEvents();
             await this.contactsContainer.fetch(credentials);
             this.contactsContainer.display();
-
         }
     }
-
 }
 
 class AccountContainer {
     username;
     identifier;
+    app;
+
+    constructor(app) {
+        this.app = app;
+    }
 
     initEvents() {
         let element = document.getElementById("account-identifier-copy");
@@ -111,12 +126,17 @@ class AccountContainer {
 
     updateAvatar() {
         let element = document.getElementById("account-img");
-        element.setAttribute("src", getAvatarImage(this.identifier));
+        element.src = getAvatarImage(this.identifier);
     }
 }
 
 class ContactsContainer {
     contacts = [];
+    app;
+
+    constructor(app) {
+        this.app = app;
+    }
 
     initEvents() {
         let searchInput = document.getElementById("search-contact-input");
@@ -143,6 +163,21 @@ class ContactsContainer {
 
         filtered.forEach((c) => {
             let element = this.contactElementFactory(c.username, c.identifier);
+            
+            element.addEventListener("click", (event) => {
+                let searchInput = document.getElementById("search-contact-input");
+                searchInput.value = "";
+                
+                this.app.chatContainer.open(c);
+                this.display();
+            })
+
+            let chatReceiver = this.app.chatContainer.receiver;
+
+            if (chatReceiver != null && chatReceiver.identifier === c.identifier) {
+                element.classList.add("selected");
+            }
+
             container.append(element);
         })
 
@@ -157,7 +192,7 @@ class ContactsContainer {
     contactElementFactory = function(username, identifier) {
         let contactImg = document.createElement("img");
         contactImg.classList.add("avatar-img");
-        contactImg.setAttribute("src", getAvatarImage(identifier));
+        contactImg.src = getAvatarImage(identifier);
 
         let contactName = document.createElement("h4");
         contactName.classList.add("name");
@@ -171,20 +206,164 @@ class ContactsContainer {
         contactContainer.classList.add("contact");
         contactContainer.id = `contact-${identifier}`;
         contactContainer.append(contactImg, contactName, contactIdentifier);
-    
+
         return contactContainer;
     }
 
 }
 
 class ChatContainer {
+    receiver;
+    history = [];
+    app;
 
+    isOpen = false;
+
+    constructor(app) {
+        this.app = app;
+    }
+
+    initEvents() {
+        let identifierCopy = document.getElementById("chat-receiver-identifier-copy");
+
+        identifierCopy.addEventListener("click", event => {
+            copyToClipboard("#" + this.receiver.identifier);
+        })
+
+        let chatInput = document.getElementById("message-input");
+        chatInput.addEventListener("keypress", function (event) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                document.getElementById("message-send-button").click();
+            }
+        });
+
+        let chatSendButton = document.getElementById("message-send-button");
+        chatSendButton.addEventListener("click", event => {
+            let input = document.getElementById("message-input");
+            
+            let text = String(input.value).trim();
+            
+            if(text !== "") { 
+                this.send(text);
+                input.value = "";
+            }
+        })
+    }
+
+    async open(contact) {
+        if (this.receiver != null && this.receiver.identifier === contact.identifier) return;
+        this.receiver = contact;
+
+        let chatInput = document.getElementById("message-input");
+        chatInput.value = "";
+
+        if (!this.isOpen) {
+            this.initEvents();
+            this.isOpen = true;
+        }
+
+        let chatWindow = document.getElementById("chat-window");
+        chatWindow.classList.add("active");
+
+        this.updateReceiverDetails();
+
+        await this.fetch();
+        this.showMessages();
+    }
+
+    async fetch() {
+        this.history = [
+            new Message(this.receiver.identifier, "This is a message!", new Date().getTime())
+        ];
+    }
+
+    updateReceiverDetails() {
+        let avatarElement = document.getElementById("chat-receiver-img")
+        avatarElement.src = getAvatarImage(this.receiver.identifier);
+
+        let nameElement = document.getElementById("chat-receiver-name")
+        nameElement.innerHTML = this.receiver.username;
+
+        let identifierElement = document.getElementById("chat-receiver-identifier")
+        identifierElement.innerHTML = "#" + this.receiver.identifier;
+    }
+
+    showMessages() {
+        let chatContainer = document.getElementById("messages");
+
+        chatContainer.replaceChildren();
+
+        this.history.forEach(msg => {
+            this.showMessage(msg, chatContainer);
+        });
+    }
+
+    addMessage(message) {
+        this.history += message;
+        this.showMessage(message);
+    }
+
+    showMessage(message, container) {
+        if (container == null) {
+            container = document.getElementById("messages");
+        }
+
+        let msgElement = this.messageElementFactory(message);
+        
+        container.append(msgElement);
+
+        msgElement.scrollIntoView({behavior: "smooth"});
+    }
+
+    messageElementFactory(message) {
+        if(message instanceof Message) {
+            let messageAvatar = document.createElement("img");
+            messageAvatar.classList.add("avatar-img");
+            messageAvatar.src = getAvatarImage(message.sender);
+
+            let messageContent = document.createElement("p");
+            messageContent.innerHTML = message.content;
+
+            let messageTimestamp = document.createElement("small");
+            
+            let date = new Date(message.timestamp);
+
+            const minutes = String(date.getMinutes()).padStart(2, '0');
+            const hours = String(date.getHours()).padStart(2, '0');
+
+            messageTimestamp.innerHTML = `${hours}:${minutes}`;
+
+            let messageContainer = document.createElement("div");
+            messageContainer.classList.add("message");
+            messageContainer.append(messageAvatar);
+            messageContainer.append(messageContent);
+            messageContainer.append(messageTimestamp);
+
+            if(!(message.sender === this.receiver.identifier)) {
+                messageContainer.classList.add("sender");
+            }
+
+           return messageContainer;
+        }
+    }
+
+    receive(message) {
+        if(message instanceof Message) {
+            this.addMessage(message);
+        }
+    }
+
+    async send(text) {
+        let message = new Message(await this.app.credentials.getHash(), text, new Date().getTime());
+        this.addMessage(message);
+    }
 }
 
+var app
 
-window.onload = function() {
-    var app = new App();
-
+window.onload = function() { 
+    app = new App();
     app.init(new Credentials("Test", "test"));
 }
 
